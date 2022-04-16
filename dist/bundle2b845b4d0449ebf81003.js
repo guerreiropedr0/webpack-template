15e4061831d96bca1897e4ee60b240d67c2094be
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-16 23:46:21 +0100

    Run build

--- dist/bundle2b845b4d0449ebf81003.js
+++ dist/bundle2b845b4d0449ebf81003.js
@@ -21,7 +21,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "", "",{"version":3,"sources":[],"names":[],"mappings":"","sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, "body {\n  background-color: #000;\n  color: #fff;\n}\n", "",{"version":3,"sources":["webpack://./src/styles/index.css"],"names":[],"mappings":"AAAA;EACE,sBAAsB;EACtB,WAAW;AACb","sourcesContent":["body {\n  background-color: #000;\n  color: #fff;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -611,4 +611,4 @@ __webpack_require__.r(__webpack_exports__);
 
 /******/ })()
 ;
-//# sourceMappingURL=bundlea3b6ec1711ab22ed9dbf.js.map
+//# sourceMappingURL=bundle2b845b4d0449ebf81003.js.map
